15398a11bfade7d023f2e9fd4bb6ad1e27c37ba3
zhangsheng <zhangsheng@huivo.com>
2016-04-13 16:14:08 +0800

    添加了多手机异步挨打方式

--- voice.py
+++ voice.py
@@ -4,19 +4,32 @@ import top.api
 url = "gw.api.taobao.com"
 #url = "www.baidu.com"
 port = 80
-appkey = "23343883"
-secret = "3b78b41ce9219f79c26cea5f0204f5c9"
+appkey = "23345058"
+secret = "049ac60eda7b6965dc75baa5bc71126b"
 
 req=top.api.AlibabaAliqinFcTtsNumSinglecallRequest(url,port)
 req.set_app_info(top.appinfo(appkey,secret))
 
-req.extend="12345"
-req.tts_param="{\"product\":\"慧沃\", \"code\":\"8888168\"}"
-req.called_num="18618458391"
-req.called_show_num="4000"
-req.tts_code="TTS_7501222"
-try:
-    resp= req.getResponse()
-    print(resp)
-except Exception,e:
-    print(e)
+def voice(phoneNo):
+    req.extend="12345"
+    req.tts_param="{\"product\":\"慧沃服务器故障语音通知\", \"code\":\"慧沃中国abcd-1234，本次通知完毕,对您的打扰表示抱歉,祝愉快\"}"
+    req.called_num= phoneNo #"13439930315"
+    req.called_show_num="4008221620"
+    req.tts_code="TTS_7730017"
+    try:
+        resp= req.getResponse()
+        print(resp)
+    except Exception,e:
+        print(e)
+
+if __name__=='__main__':
+    phoneList = {
+                    '13439930315':'秋林',
+                    '18618458391':'张胜',
+                    '13141163261':'吴志清',
+                    '13693567420':'武举光',
+                    '18601907819':'宁思平',
+                    '18601086705':'王恩东'
+                 }
+    for phone in phoneList:
+        voice(phone)
